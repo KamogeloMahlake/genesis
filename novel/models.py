b77b5183e2c716a415f31757c102ad610ee50a2a
KamogeloMahlake <3949843@myuwc.ac.za>
2026-06-03 17:56:17 +0200

    fix: update requirements.txt

--- novel/models.py
+++ novel/models.py
@@ -52,7 +52,7 @@ class Novel(models.Model):
         blank=True,
         null=True,
     )
-    creator = models.CharField(max_length=50, default="Admin", blank=True, null=True)
+    creator = models.CharField(default="Admin", blank=True, null=True)
     title = models.CharField(max_length=1000, unique=True)
     description = models.TextField(blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
